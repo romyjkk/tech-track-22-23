fe91fb01d6ec2d59d5196f1efd20d3a33a7ad03f
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-11 13:47:51 +0100

    Leaflet map toegevoeg

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,16 @@ import * as d3 from 'd3';
 
 console.log('Hello, world!');
 
+// importing the data from my data.json file and logging it to the console
+
 import myJson from '../data.json' assert {type: 'json'};
 
 console.log(myJson);
+
+// test map
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
