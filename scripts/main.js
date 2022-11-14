aaf27d8b97058660c9e69cbf0c5b8862e9f392df
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-14 11:53:49 +0100

    Gekut met Leaflet en Greensock

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,8 @@ console.log('Hello, world!');
 
 // importing the data from my data.json file and logging it to the console
 
-import myJson from '../data.json' assert {type: 'json'};
+// import myJson from '../data.json' assert {type: 'json'};
+import myJson from '../geoJSON.json' assert {type: 'json'};
 
 console.log(myJson);
 
@@ -37,21 +38,41 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// popup
+// geoJSON
 
-// let marker = L.marker([9.005401, 38.763611]).addTo(map);
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
 
-// let popup = L.popup()
-//     .setLatLng([9.005401, 38.763611])
-//     .setContent("The Abyssinian, Ethiopia")
-//     .openOn(map);
+// icons
 
-let abyssinianIcon = L.icon({
-    iconUrl: '../images/png/abyssinian.png',
+// let abyssinianIcon = L.icon({
+//     iconUrl: '../images/png/abyssinian.png',
 
-    iconSize: [60, 62],
-    iconAnchor:   [5, 40],
-    popupAnchor:  [-2, -76]
-})
+//     iconSize:     [50, 80],
+//     iconAnchor:   [22, 94],
+//     popupAnchor:  [-3, -76]
+    
+//     iconSize: [60, 62],
+//     iconAnchor:   [5, 40],
+//     popupAnchor:  [-10, 5]
+// })
 
-L.marker([9.005401, 38.763611], {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian"); 
+// L.marker([9.005401, 38.763611], {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian");
+
+// L.marker([9.005401, 38.763611]).addTo(map)
+//     .bindPopup('The Abyssinian')
+//     .openPopup();
+
+// practising with greensock
+
+// const tl = gsap.timeline()
+//     .from(".title", {xPercent:-150, ease:"back", duration:1})
+//     .from(".cartooncat", {xPercent:150, ease:"back", duration:1})
