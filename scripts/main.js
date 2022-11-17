259d1bc4dee18d6e58c7a4fdb7867f19ad98e3bb
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-17 11:52:06 +0100

    Volgorde van de data fixen

--- scripts/main.js
+++ scripts/main.js
@@ -18,15 +18,19 @@ import '../styles/style.css'
 
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
+// let katten = d3.json("../coordinates.json")
+// .catch(e => console.log("laden niet gelukt"));
+// console.log(katten[0]);
 
 console.log('Hello, world!');
 
 // importing the data from my data.json file and logging it to the console
 
 // import myJson from '../data.json' assert {type: 'json'};
-import myJson from '../geoJSON.json' assert {type: 'json'};
+import myJson from '../coordinates.json' assert {type: 'json'};
+console.log(myJson[0].Coordinates[0]);
 
-console.log(myJson);
+// console.log(myJson);
 
 // test map
 
@@ -38,64 +42,109 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// geoJSON
+// geoJSON ???
 
-var myCoords = [{
-    "type": "Point",
-    "coordinates": [38.763611, 9.005401]
-}, {
-    "type": "Point",
-    "coordinates": [-0.118092, 51.509865]
-}, {
-    "type": "Point",
-    "coordinates": [105.3809, 62.2270]
-}, {
-    "type": "Point",
-    "coordinates": [-98.000000, 38.500000]
-}, {
-    "type": "Point",
-    "coordinates": [100.9925, 15.8700]
-}, {
-    "type": "Point",
-    "coordinates": [53.6880, 32.4279]
-}];
+// var myCoords = [{
+//     "type": "Point",
+//     "coordinates": [38.763611, 9.005401]
+// }, {
+//     "type": "Point",
+//     "coordinates": [-0.118092, 51.509865]
+// }, {
+//     "type": "Point",
+//     "coordinates": [105.3809, 62.2270]
+// }, {
+//     "type": "Point",
+//     "coordinates": [-98.000000, 38.500000]
+// }, {
+//     "type": "Point",
+//     "coordinates": [100.9925, 15.8700]
+// }, {
+//     "type": "Point",
+//     "coordinates": [53.6880, 32.4279]
+// }];
 
-var myLayer = L.geoJSON(myCoords).addTo(map);
-myLayer.addData(geojsonFeature);
+// var myLayer = L.geoJSON(myCoords).addTo(map);
+// myLayer.addData(someGeojsonFeature);
 
 
 // custom marker
 
-// const geojsonMarkerOptions = {
+// var geojsonMarkerOptions = {
 //     radius: 8,
 //     fillColor: "#ff7800",
 //     color: "#000",
 //     weight: 1,
 //     opacity: 1,
 //     fillOpacity: 0.8
-// }
+// };
 
-// L.geoJSON(geojsonFeature, {
-//     pointToLayer: function (myCoords, latlng) {
+// L.geoJSON(someGeojsonFeature, {
+//     pointToLayer: function (points, latlng) {
 //         return L.circleMarker(latlng, geojsonMarkerOptions);
 //     }
 // }).addTo(map);
 
 // icons
 
-// let abyssinianIcon = L.icon({
-//     iconUrl: '../images/png/abyssinian.png',
+let abyssinianIcon = L.icon({
+    iconUrl: '../images/png/abyssinian.png',
 
-//     iconSize:     [50, 80],
-//     iconAnchor:   [22, 94],
-//     popupAnchor:  [-3, -76]
-    
-//     iconSize: [60, 62],
-//     iconAnchor:   [5, 40],
-//     popupAnchor:  [-10, 5]
-// })
+    iconSize:     [65, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [25, -100]
+})
 
-// L.marker([9.005401, 38.763611], {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian");
+let britishshorthairIcon = L.icon({
+    iconUrl: '../images/png/britishshorthair.png',
+
+    iconSize:     [80, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [15, -100]
+})
+
+let ragdollIcon = L.icon({
+    iconUrl: '../images/png/ragdoll.png',
+
+    iconSize:     [90, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [28, -100]
+})
+
+let sphynxIcon = L.icon({
+    iconUrl: '../images/png/sphynx.png',
+
+    iconSize:     [80, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [40, -100]
+})
+
+let persianIcon = L.icon({
+    iconUrl: '../images/png/persian.png',
+
+    iconSize:     [80, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [43, -100]
+})
+
+let siameseIcon = L.icon({
+    iconUrl: '../images/png/siamese.png',
+
+    iconSize:     [80, 80],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [20, -100]
+})
+
+L.marker(myJson[0].Coordinates, {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian, from Ethiopia");
+L.marker(myJson[1].Coordinates, {icon:britishshorthairIcon}).addTo(map).bindPopup("The British Shorthair, from England");
+L.marker(myJson[2].Coordinates, {icon:persianIcon}).addTo(map).bindPopup("The Persian, from Iran");
+L.marker(myJson[3].Coordinates, {icon:ragdollIcon}).addTo(map).bindPopup("The Ragdoll, from the United States");
+L.marker(myJson[4].Coordinates, {icon:siameseIcon}).addTo(map).bindPopup("The Siamese, from Thailand");
+L.marker(myJson[5].Coordinates, {icon:sphynxIcon}).addTo(map).bindPopup("The Sphynx, from Canada");
+
+// met d3 alles tegelijk inladen + join (moet kunnen)
+
+
 
 // L.marker([9.005401, 38.763611]).addTo(map)
 //     .bindPopup('The Abyssinian')
@@ -106,3 +155,4 @@ myLayer.addData(geojsonFeature);
 // const tl = gsap.timeline()
 //     .from(".title", {xPercent:-150, ease:"back", duration:1})
 //     .from(".cartooncat", {xPercent:150, ease:"back", duration:1})
+
