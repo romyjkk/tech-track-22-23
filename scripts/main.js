3ffa50845d2561ef7ebed425825d91e4402fa24e
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-18 11:38:23 +0100

    D3 gebruikt om mijn data in te laden eindelijk gelukt woeee joepie

--- scripts/main.js
+++ scripts/main.js
@@ -5,21 +5,16 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 
 // importing json file using d3
-// let allCats = d3.json("../coordinates.json");
 
-// console.log(allCats[0]);
-
-
-// let allCats = d3.json("../coordinates.json", function(error, data){
-//     console.log("allCats"[0]);
-// }).catch(e => console.log("laden niet gelukt"));
+d3.json("../coordinates.json").then(d => console.log(d[0]))
+    .catch(e => console.log("Laden niet gelukt"));
 
 // importing the data from my data.json file and logging it to the console
 
 // import myJson from '../data.json' assert {type: 'json'};
-import myJson from '../coordinates.json' assert {type: 'json'};
-console.log(myJson);
-console.log(myJson[0].Coordinates[0]);
+// import myJson from '../coordinates.json' assert {type: 'json'};
+// console.log(myJson);
+
 
 // map
 
@@ -81,9 +76,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     popupAnchor:  [20, -100]
 // })
 
-// L.marker(myJson[0].Coordinates, {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian, from Ethiopia");
-// L.marker(myJson[1].Coordinates, {icon:britishshorthairIcon}).addTo(map).bindPopup("The British Shorthair, from England");
-// L.marker(myJson[2].Coordinates, {icon:persianIcon}).addTo(map).bindPopup("The Persian, from Iran");
-// L.marker(myJson[3].Coordinates, {icon:ragdollIcon}).addTo(map).bindPopup("The Ragdoll, from the United States");
-// L.marker(myJson[4].Coordinates, {icon:siameseIcon}).addTo(map).bindPopup("The Siamese, from Thailand");
-// L.marker(myJson[5].Coordinates, {icon:sphynxIcon}).addTo(map).bindPopup("The Sphynx, from Canada");
+// L.marker(allCats[0], {icon:abyssinianIcon}).addTo(map).bindPopup("The Abyssinian, from Ethiopia");
+// L.marker(myJson[1].geometry.coordinates, {icon:britishshorthairIcon}).addTo(map).bindPopup("The British Shorthair, from England");
+// L.marker(myJson[2].geometry.coordinates, {icon:persianIcon}).addTo(map).bindPopup("The Persian, from Iran");
+// L.marker(myJson[3].geometry.coordinates, {icon:ragdollIcon}).addTo(map).bindPopup("The Ragdoll, from the United States");
+// L.marker(myJson[4].geometry.coordinates, {icon:siameseIcon}).addTo(map).bindPopup("The Siamese, from Thailand");
+// L.marker(myJson[5].geometry.coordinates, {icon:sphynxIcon}).addTo(map).bindPopup("The Sphynx, from Canada");
