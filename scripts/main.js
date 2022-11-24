536da75e4d645823af2e8795ac4c158249bda7e6
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-24 16:54:13 +0100

    Loop werkt opeens?

--- scripts/main.js
+++ scripts/main.js
@@ -7,33 +7,52 @@ import * as d3 from 'd3';
 
 // importing json file using d3
 
-// d3.json("../coordinates.json").then(d => {
-//     makeMap(d)
-//     console.log(d)
-// })
-
-// let allCats = d3.json("../coordinates.json").then(d => console.log(d))
-//     .catch(e => console.log("Laden niet gelukt"));
-
-d3.json("../popularity.json").then(d => {
-    makeChart(d)
+d3.json("../coordinates.json").then(d => {
+    makeMap(d)
     console.log(d)
 })
 
+// d3.json("../popularity.json").then(d => {
+//     makeChart(d)
+//     updateChart(d)
+//     filterData(d)
+//     console.log(d)
+// })
+
 // d3 barchart
 
-function makeChart(catPopularity) {
+// var objSet;
+// var sqrtScale;
 
-    const margin = 60;
-    const width = 1000 - 2 * margin;
-    const height = 600 - 2 * margin;
+// function updateChart(categoryNumber) {
+//     categoryNumber = Number(categoryNumber);
+//     const catPopularity = filterData(categoryNumber);
+//     makeChart(catPopularity);
+// }
 
-    const svg = d3.select("svg");
+// function filterData(categoryNumber) {
 
-    const chart = svg.append("g")
-        .attr("transform", `translate(${margin}, ${margin})`);
+//     objSet = catPopularity.filter((d) => d.category === categoryNumber);
 
-    const yScale = d3.scaleLinear()
+//     sqrtScale = d3.scaleSqrt()
+//         .domain([0, d3.max(catPopularity, (d) => d.breed)])
+//         .range([0, 10]);
+
+//         return objSet;
+// }
+
+// function makeChart(catPopularity) {
+
+//     const margin = 60;
+//     const width = 1000 - 2 * margin;
+//     const height = 600 - 2 * margin;
+
+//     const svg = d3.select("svg");
+
+//     const chart = svg.append("g")
+//         .attr("transform", `translate(${margin}, ${margin})`);
+
+//     const yScale = d3.scaleLinear()
         // .range([height, 0])
         // .domain([0, 10]);
 
@@ -41,65 +60,68 @@ function makeChart(catPopularity) {
         // .call(d3.axisLeft(yScale));
 
 
-    const xScale = d3.scaleBand()
-        .range([0, width])
-        .domain(catPopularity.map((d) => d.place))
-        .padding(0.2)
+//     const xScale = d3.scaleBand()
+//         .range([0, width])
+//         .domain(catPopularity.map((d) => d.place))
+//         .padding(0.2)
 
-    chart.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(xScale))
+//     chart.append("g")
+//         .attr("transform", `translate(0, ${height})`)
+//         .call(d3.axisBottom(xScale))
 
-    //     const tooltip =
-    //   d3.select('body').append('div')
-    //     .attr('class', 'tooltip')
-    //     .style('opacity', 0)
+//     chart.selectAll()
+//         .data(catPopularity)
+//         .enter()
+//         .append("rect")
+//         .attr("x", (d) => xScale(d.place))
+//         .attr("y", (d) => yScale(d.place))
+//         .attr("height", (d) => height - yScale(d.place))
+//         .attr("width", xScale.bandwidth())
+//         .attr("fill", '#fdcbd2')
+//             .on("mouseover touchstart", (e, d) =>
+//             d3
+//                 .select("#tooltip")
+//                 .transition()
+//                 .duration(200)
+//                 .style("opacity", 1)
+//                 .text(`${d.breed}`)
+//                 )
 
-    chart.selectAll()
-        .data(catPopularity)
-        .enter()
-        .append("rect")
-        .attr("x", (d) => xScale(d.place))
-        .attr("y", (d) => yScale(d.place))
-        .attr("height", (d) => height - yScale(d.place))
-        .attr("width", xScale.bandwidth())
-        .attr("fill", '#000000')
-            .on("mouseover touchstart", (e, d) =>
-            d3
-                .select("#tooltip")
-                .transition()
-                .duration(200)
-                .style("opacity", 1)
-                .text(`${d.breed}`)
-                )
+//             .on("mousemove", (e) =>
+//                 d3
+//                     .select("#tooltip")
+//                     .style("left", e.pageX + 15 + "px")
+//                     .style("top", e.pageY + 15 + "px")
+//                     )
 
-            .on("mousemove", (e) =>
-                d3
-                    .select("#tooltip")
-                    .style("left", e.pageX + 15 + "px")
-                    .style("top", e.pageY + 15 + "px")
-                    )
+//             .on("mouseout", e => d3.select("#tooltip").style("opacity", 0)
+//                     );
 
-            .on("mouseout", e => d3.select("#tooltip").style("opacity", 0)
-                    );
+//     svg.append("text")
+//         .attr("x", width / 2 + margin)
+//         .attr("y", 600)
+//         .attr("text-anchor", "middle")
+//         .text("Cat Breeds")
+// }
 
-    svg.append("text")
-        .attr("x", width / 2 + margin)
-        .attr("y", 600)
-        .attr("text-anchor", "middle")
-        .text("Cat Breeds")
-}
+// function ticked() {
+//     window.addEventListener("DOMContentLoaded", (e) => {
+//         d3.selectAll("button").on("click", (e) =>
+//         updateChart(e.target.value));
+//         updateChart(1);
+//     });
+// }
 
 // map
 
-// function makeMap(allCats) {
-// const map = L.map('map').setView([52.377956, 4.897070], 2);
+function makeMap(allCats) {
+const map = L.map('map').setView([52.377956, 4.897070], 2);
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     minZoom: 2,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 2,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 // // custom icons
 
@@ -113,11 +135,11 @@ function makeChart(catPopularity) {
 
 // L.marker(allCats[1].geometry.coordinates).addTo(map).bindPopup(allCats[1].properties.name)
 
-// function makeMarker() {
-//     allCats.forEach(allCats => {
-//         L.marker(allCats.geometry.coordinates).addTo(map).bindPopup(allCats.properties.name)
-//     });
-    
-// makeMarker();
+function makeMarker() {
+    allCats.forEach(allCats => {
+        L.marker(allCats.geometry.coordinates).addTo(map).bindPopup(allCats.properties.name)
+    });
+}
+makeMarker();
 
-// }}
+}
