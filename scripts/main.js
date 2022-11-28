048410433192e2275e432861073e0602496c9536
romyjkk <98404201+romyjkk@users.noreply.github.com>
2022-11-28 17:03:15 +0100

    Barchart gefixt :)

--- scripts/main.js
+++ scripts/main.js
@@ -22,47 +22,38 @@ function getData(filterFunction) {
                 return item.category === filterFunction;
             })
         }
+
         makeChart(d)
-        // updateChart(d)
-        // filterData(d)
     })
 }
 
 // d3 barchart
 
-// var objSet;
-// var sqrtScale;
-
-// function updateChart(categoryCountry) {
-//     categoryCountry = String(categoryCountry);
-//     const catPopularity = filterData(categoryCountry);
-//     makeChart(catPopularity);
-// }
-
-// function filterData(categoryCountry) {
-
-//     objSet = catPopularity.filter((d) => d.category === categoryCountry);
-
-//     // sqrtScale = d3.scaleSqrt()
-//     //     .domain([0, d3.max(catPopularity, (d) => d.breed)])
-//     //     .range([0, 10]);
-
-//         return objSet;
-// }
-
-function makeChart(catPopularity) {
-
-    // console.log('werkt dit uberhaupt')
-
-    const margin = 60;
-    const width = 1000 - 2 * margin;
+    const margin = 50;
+    const width = screen.width - 2 * margin;
     const height = 600 - 2 * margin;
 
     const svg = d3.select(".barchart svg");
 
+function initChart() {
     const chart = svg.append("g")
-        .attr("transform", `translate(${margin}, ${margin})`);
+    .attr("transform", `translate(${margin}, ${margin})`)
+    .attr("id", "container")
 
+    chart.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .attr("id", "xScale")
+    // .call(d3.axisBottom(xScale))
+
+    svg.append("text")
+        .attr("x", width / 2 + margin)
+        .attr("y", 600)
+        .attr("text-anchor", "middle")
+        .text("Cat Breeds")
+        .attr("id", "title")
+}
+
+function makeChart(catPopularity) {
     const yScale = d3.scaleLinear()
         // .range([height, 0])
         // .domain([0, 10]);
@@ -74,21 +65,24 @@ function makeChart(catPopularity) {
     const xScale = d3.scaleBand()
         .range([0, width])
         .domain(catPopularity.map((d) => d.place))
-        .padding(0.2)
+        .padding(0.05)
 
-    chart.append("g")
-        .attr("transform", `translate(0, ${height})`)
+        const chart = d3.select("#container");
+
+    chart.select("#xScale")
+        // .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(xScale))
 
-    chart.selectAll()
+    chart.selectAll("rect")
         .data(catPopularity)
-        .enter()
-        .append("rect")
+        .join("rect")
         .attr("x", (d) => xScale(d.place))
         .attr("y", (d) => yScale(d.place))
         .attr("height", (d) => height - yScale(d.place))
         .attr("width", xScale.bandwidth())
-        // .attr("fill", (d) => `url(#${d.image})`)
+        // .transition()
+        // .duration(500)
+        .attr("fill", (d) => `url(#${d.image})`)
             .on("mouseover touchstart", (e, d) =>
             d3
                 .select("#tooltip")
@@ -106,11 +100,9 @@ function makeChart(catPopularity) {
             .on("mouseout", e => d3.select("#tooltip").style("opacity", 0)
                     );
 
-    svg.append("text")
-        .attr("x", width / 2 + margin)
-        .attr("y", 600)
-        .attr("text-anchor", "middle")
-        .text("Cat Breeds")
+
+        d3.select("#title")
+            .text(catPopularity[0].category)
 }
 
 window.addEventListener("DOMContentLoaded", (e) => {
@@ -119,4 +111,5 @@ window.addEventListener("DOMContentLoaded", (e) => {
     getData("The World");
 });
 
+initChart();
 getData();
